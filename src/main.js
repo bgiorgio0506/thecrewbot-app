59777f996be1ce40afe52c38b1e27fc8c8c89cf9
Giorgio <bgiorgio0506@gmail.com>
2020-08-23 18:26:28 +0200

    fix autoupdater and started laying out UI

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
 'use strict'
 // Import parts of electron to use
-const { app, BrowserWindow, autoUpdater, ipcMain, nativeImage} = require('electron')
+const { app, BrowserWindow, autoUpdater, ipcMain, nativeImage, dialog} = require('electron')
 require('update-electron-app')()
 const utils = require('../src/helpers/utility')
 const tmi = require('tmi.js')
 const path = require('path');
 const log  = require('electron-log')
+const settings = require('electron-settings')
 // Add React extension for development
 const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let gotTheLock
+
 // Keep a reference for dev mode
 let dev = false
+
+//Init settings
+settings.set('config.prefix', "!").catch(err=>{throw err});
+settings.set('config.channel', ['paolom346_']);
+settings.set('config.updateLater', false);
+
 //Question queue
 let questQueue = [];
 let id = 0;
@@ -27,7 +36,24 @@ if (process.platform === 'win32') {
   app.commandLine.appendSwitch('high-dpi-support', 'true')
   app.commandLine.appendSwitch('force-device-scale-factor', '1')
 }
-console.log(path.join(__dirname,'assets/icons/ico/ico.svg'))
+//console.log(path.join(__dirname,'assets/icons/ico/ico.svg'))
+
+
+/**
+ * SingleInstace
+ */
+//Make single instance here before the whole thing loads!!
+if (gotTheLock === false) {
+  app.quit();
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus();
+    }
+  })
+}
+
 
 /*** MAIN Window creation ****/
 function createWindow() {
@@ -64,26 +90,6 @@ function createWindow() {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
 
 /** CHAT BOT SECTION **/
 //chatbot opts 
@@ -97,15 +103,25 @@ connection: {
       username: process.env.TWITCH_BOT_USERNAME,
       password: process.env.TWITCH_BOT_PASSWORD,
   },
-  channels:['paolom346_']
+  channels:settings.getSync('config.channel')
 })
 
 ClientBot.connect().catch((error)=>{throw error;})
 
 ClientBot.on('message', (channel, tags, message, self)=>{
-  
+
+  //Command sys
+  let prefix = settings.getSync('config.prefix')// get settings 
+  let messageArr = message.split(' ')//split the message into array
+
+  //get only bot command
+  console.log(messageArr[0].includes('!'))
+  if(messageArr[0].includes(prefix) !== true) return;
+    const args = messageArr[0].slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
   //log.info(message, tags, channel, self)
-  if(message.includes('!domanda') === true){
+  if(command === 'domanda'){
       let questObj = {}
       let messageArr = message.split(' '); // split the string into an Array
       messageArr.shift();// remove command string from message content
@@ -136,7 +152,7 @@ ClientBot.on('message', (channel, tags, message, self)=>{
   }
 
   //status command
-  if(message.includes('!status') === true){
+  if(command === "status"){
     if (questQueue.length > 0) {
      let index = utils.findIndexInObjArr(questQueue, 'user', tags.username) //find the question in the erray
      if (index === -1) {
@@ -148,11 +164,7 @@ ClientBot.on('message', (channel, tags, message, self)=>{
     }else  ClientBot.say(channel,'Non abbiamo trovato la tua domanda')
   }
 
-  if(message.includes('!social') === true){
-    ClientBot.say(channel, 'Trovami in tutti i social quì: https://linktr.ee/paolom346')
-  }
-
-  if(message.includes('!donazioni') === true){
+  if(command === "donate"){
     ClientBot.say(channel, 'Il link per le donazioni è https://streamlabs.com/paolom346_/tip '+'  Grazie per il supporto '+ tags.username+ ' !!')
   }
 })
@@ -166,13 +178,55 @@ ClientBot.on('connected', ()=>{
 
 
 /*** EVENT HANDLING AND IPC CALLS ***/
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
 //when a question is marked as completed then delete the element
 ipcMain.on('rm-quest', (e ,id)=>{
   questQueue = questQueue.filter(item => item.id !== id)
   //console.log(questQueue.length) //debug
 })
 
+//Second instance 
+app.on('second-instance', (event, commandLine, workingDirectory) => {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore()
+      mainWindow.focus();
+    }
+    else
+      event.preventDefault()
+    mainWindow.show()
+  }
+})
 
+
+ipcMain.on('quit-app', function () {
+  app.quit();
+});
+
+
+/**END IPC SECTION */
 
 
 /** UPDATER SECTION **/
@@ -184,6 +238,39 @@ if(dev === false){
 
   setInterval(() => {
     autoUpdater.checkForUpdates()
+    log.info('Checking Update...')
   }, 10 * 60 * 1000)
+
+  autoUpdater.on("error", error => {
+    log.error("AutoUpdater Error: ");
+    log.error(error.message);
+    log.error(error.stack);
+    log.error(dialog.showErrorBox("Error!", error.message));
+  });
+
+  autoUpdater.on("update-not-available", info => {
+    log.info("update not available");
+  });
+
+  
+  autoUpdater.on("checking-for-update", () => {
+    log.info("checking for update");
+  });
+
+  autoUpdater.on('update-available', () => {
+    log.info("update available!");
+
+    autoUpdater.on('update-downloaded', function (event, releaseName) {
+      // # restart app, then update will be applied
+      log.info("update downloaded!");
+    if(settings.getSync('config.updateLater')=== true)return;
+      dialog.showMessageBox({type:'info', buttons:['Update now', 'Update Later'], title:'Update avaliable', message:'An update is available would you like to update?'}).then((res)=>{
+        if(res === 0 ) return autoUpdater.quitAndInstall();
+        if(res === 1) settings.set('config.updateLater', true)
+      })
+    });
+
+  });
+
 
 }
